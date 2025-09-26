88eed074cd34726229c2297577513d9af86622e1
Harishanan <ht7998v@gre.ac.uk>
2025-09-26 20:16:36 +0100

    Finished Login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
-import Dashboard from './pages/admin/Dashboard'
+import Login from './components/admin/Login'
 import Layout from './pages/admin/Layout'
+import Dashboard from './pages/admin/Dashboard'
 import AddBlog from './pages/admin/AddBlog'
 import ListBlog from './pages/admin/ListBlog'
 import Comments from './pages/admin/Comments'
@@ -13,7 +14,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
-        <Route path='/admin' element={<Layout />}>
+        <Route path='/admin' element={true ? <Layout /> : <Login />}>
           <Route index element={<Dashboard />} />
           <Route path='addBlog' element={<AddBlog />}/>
           <Route path='listBlog' element={<ListBlog />}/>
